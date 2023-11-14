641b24123e88f0e272fc8529c266c58643a46e34
aymentkr <aymentkr@gmail.com>
2023-11-14 17:09:10 +0100

    Switches Implementation #3

--- src/app/inlet/inlet.component.ts
+++ src/app/inlet/inlet.component.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {DataService} from "../services/data.service";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-inlet',
@@ -11,7 +12,6 @@ export class InletComponent implements OnInit{
   isLoading: boolean = true;
   size = 0;
   formData = {
-    id : 0,
     preferredInlet: 0,
     BypassSelectedInlet: 0,
     BypassActiveInlet: 0,
@@ -35,31 +35,68 @@ export class InletComponent implements OnInit{
       console.error('Data fetching failed:', error);
     });
   }
-
   async fetchData(){
     this.size = parseFloat(await this.data.getResult('#inlets', 'print(#inlets)'));
     const test = parseFloat(await this.data.getResult(`inlets[1]:voltage(0)`,`print(inlets[1]:getVoltage(0))`));
     this.hasPoles = !isNaN(test);
     await this.fetchSwitchData();
   }
-
   async fetchSwitchData() {
     const size = parseFloat(await this.data.getResult('#switches', 'print(#switches)'));
     if (size ===1){
-      this.formData.id = 1;
       this.formData.preferredInlet = parseFloat(await this.data.getResult(`preferredInlet`, `print(switches[1]:getPreferredInlet())`));
       this.formData.BypassSelectedInlet = parseFloat(await this.data.getResult(`BypassSelectedInlet`, `print(switches[1]:getBypassSelectedInlet())`));
       this.formData.BypassActiveInlet = parseFloat(await this.data.getResult(`BypassActiveInlet`, `print(switches[1]:getBypassActiveInlet())`));
     }
   }
-
-
   submitForm(formData: any) {
     console.log(formData)
   }
 
+  resetFormData() {
+    this.formData = {
+      preferredInlet: 0,
+      BypassSelectedInlet: 0,
+      BypassActiveInlet: 0,
+      FaultFlags: 0,
+      Inlet1FaultFlags: 0,
+      Inlet2FaultFlags: 0,
+      InletPhaseAngle: 0,
+      PowerFailDetectTime: 0,
+      RelayOpenTime: 0,
+      TotalTransferTime: 0,
+    };
+  }
   generateInletRange(): number[] {
     return Array.from({ length: this.size }, (_, index) => index + 1);
   }
 
+  info() {
+    Swal.fire({
+      title: 'switches[1..1]:methods',
+      html: '<ul>' +
+        '  <li><mark>Sets fault flags</mark>  <pre> (1=phase, 2=overload) </pre> </li>' +
+        '  <li><mark>Sets Inlet 1 fault flags </mark> </li>' +
+        '  <li><mark>Sets Inlet 2 fault flags </mark>  <pre>(1=SMPS, 2=MOV, 4=short, 8=open) </pre> </li>' +
+        '  <li><mark>Sets Inlet phase angle</mark>   <pre>to 15 f.e </pre> </li>' +
+        `  <li><mark>Sets preferred Inlet </mark>  <pre> [1 .. ${this.size}] </pre> </li>` +
+        '  <li><mark>Sets power fail detect time</mark> </li>' +
+        '  <li><mark>Sets relay open time</mark></li>' +
+        '  <li><mark>Sets total transfer time</mark> <pre>  (microseconds) </pre> </li>' +
+        `  <li><mark>Sets bypass selected Inlet </mark>   <pre>[0=none, 1 .. ${this.size}] </pre> </li>` +
+        '</ul>',
+      icon: 'info',
+    });
+
+  }
+
+  seeDetails() {
+    Swal.fire({
+      title: 'Switches[1..1]:value ',
+      html: `<mark> Preffered Inlet: </mark> <pre> ${this.formData.preferredInlet} </pre>`+
+            `<mark> Bypass Selected Inlet: </mark> <pre> ${this.formData.BypassSelectedInlet} </pre>`+
+            `<mark> Bypass Active Inlet: </mark> <pre> ${this.formData.BypassActiveInlet} </pre>`,
+      icon: 'info'
+    });
+  }
 }
