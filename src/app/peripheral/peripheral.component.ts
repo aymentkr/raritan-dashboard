d9386fda384663f497f0c992dafa131a18a3447b
aymentkr <aymentkr@gmail.com>
2023-11-21 13:56:28 +0100

    Create virtualDevicesChart

--- src/app/peripheral/peripheral.component.ts
+++ src/app/peripheral/peripheral.component.ts
@@ -1,6 +1,7 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, NgZone, OnInit} from '@angular/core';
-import {MatTableDataSource} from "@angular/material/table";
-import {VPD} from "../model/interfaces";
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, NgZone } from '@angular/core';
+import { MatTableDataSource } from "@angular/material/table";
+import { VPD } from "../model/interfaces";
+import * as Chart from 'chart.js';
 
 @Component({
   selector: 'app-peripheral',
@@ -8,18 +9,41 @@ import {VPD} from "../model/interfaces";
   styleUrls: ['./peripheral.component.css'],
   changeDetection: ChangeDetectionStrategy.Default,
 })
-export class PeripheralComponent implements OnInit{
+export class PeripheralComponent implements AfterViewInit {
+  chart: any;
   dataSource = new MatTableDataSource<VPD>();
-  Columns: string[] = ['peripheral_id','device_name','device_type','sensor_type','serial_number']
+  Columns: string[] = ['peripheral_id', 'device_name', 'device_type', 'sensor_type', 'serial_number'];
 
-  constructor( private ngZone: NgZone, private cdr: ChangeDetectorRef) {
+  constructor(private ngZone: NgZone, private cdr: ChangeDetectorRef) {}
 
-  }
-
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.ngZone.run(() => {
+      this.createChart();
       this.cdr.detectChanges();
-    })
+    });
+  }
+  createChart() {
+    this.chart = new Chart('InletChart', {
+      type: 'line',
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: 'Frequency Hz',
+            data: [],
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        scales: {
+
+        },
+      },
+    });
   }
 
 
